359b09aa9d68d7a1021b6789c7f327d5e1243887
Yifan Song <yifans@mail.ustc.edu.cn>
2018-05-16 15:17:59 +0800

    add help pages

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="help">
-    help
-    <h1>TODO</h1>
+    <br>
+    <h1>系统说明</h1>
+    该系统用于IRFEL项目历史数据查询与展示，目前使用HLS-II数据进行系统测试。
+    <ul>
+      <li>后端： 后端采用The EPICS Archiver Appliance进行历史数据采集。</li>
+      <li>前端： 前端基于Vue.js2.0开发了单页应用(SPA)。</li>
+    </ul>
+    <h1>其它</h1>
+    <ul>
+      <li>如有任何问题，请联系 <a href="mailto:yifans@mail.ustc.edu.cn">yifans@mail.ustc.edu.cn。</a>
+        或者 <a href="mailto:gfliu@ustc.edu.cn">gfliu@ustc.edu.cn</a></li>
+      <li>代码托管于 <a href="https://github.com/yifans/irfel-data-query">https://github.com/yifans/irfel-data-query</a>。</li>
+    </ul>
   </div>
 </template>
 
